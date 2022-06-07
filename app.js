76f8bc90c2ed9f076d5204ea37fa7c69be14b740
Alexandros <alexandr.kyriakou@gmail.com>
2022-06-07 19:37:32 +0300

    nav-mobile functionality and style

---
+++ app.js
@@ -0,0 +1,23 @@
+const menuToggleIcon = document.getElementById("menu-toggle-icon");
+const navMobile = document.getElementById("nav");
+const headerElement = document.getElementById("header");
+
+// Toggle Navigation
+const toggleMenu = () => {
+  navMobile.classList.toggle("active");
+  headerElement.classList.toggle("active");
+};
+
+menuToggleIcon.addEventListener("click", toggleMenu);
+
+// add/remove border-bottom when scroll
+
+const headerBorderScroll = () => {
+  if (this.scrollY > 30) {
+    headerElement.classList.add("active-scroll");
+  } else {
+    headerElement.classList.remove("active-scroll");
+  }
+};
+
+window.addEventListener("scroll", headerBorderScroll);
